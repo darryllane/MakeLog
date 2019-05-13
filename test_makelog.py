2f8e74b7d624e3d8c38011c52b880176b876929c
Darryl Lane <darryllane101@gmail.com>
2019-05-14 00:18:30 +0100

    unittest

--- test_makelog.py
+++ test_makelog.py
@@ -85,6 +85,37 @@ class TestMakeLog(unittest.TestCase):
 			successful = self.checkout(self.debug)
 			self.assertTrue(successful, True)
 			
+	
+	def test_error_custom2(self):
+		print('\n\nTesting custom error log with prefixed /')
+		logger = log.initialise(LOG_DIR='~/custom_log/', ERROR_LOG_FILE = '/error_log2.log')
+		logger.error('error_test')
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore")		
+			self.error = self.read(logger.ERROR_LOG_FILE)
+			successful = self.checkout(self.error)
+			self.assertTrue(successful, True)
+
+	def test_info_custom2(self):
+		print('\n\nTesting custom info log with prefixed /')
+		logger = log.initialise(LOG_DIR='~/custom_log/', INFO_LOG_FILE = '/information_log2.log')
+		logger.info('info_test')
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore")		
+			self.info = self.read(logger.INFO_LOG_FILE)		
+			successful = self.checkout(self.info)
+			self.assertTrue(successful, True)
+
+	def test_debug_custom2(self):
+		print('\n\nTesting custom debug log with prefixed /')
+		logger = log.initialise(LOG_DIR='~/custom_log/', DEBUG_LOG_FILE = '/debug_log2.log')
+		logger.debug('debug_test')
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore")		
+			self.debug = self.read(logger.DEBUG_LOG_FILE)
+			successful = self.checkout(self.debug)
+			self.assertTrue(successful, True)			
+			
 
 if __name__ == '__main__':
 	unittest.main()
